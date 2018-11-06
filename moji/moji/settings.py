0bb2ce1e4d75b4625525d14f2b3910b558f6b27f
simeon <sajala8624@gmail.com>
2018-11-06 12:34:31 -0500

    'tenp'

--- moji/moji/settings.py
+++ moji/moji/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'moji.middleware.LoginRequiredMiddleware',
+    #'moji.middleware.LoginRequiredMiddleware',
     #'moji.middleware.BlockedMiddleware',
 ]
 
